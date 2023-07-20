bb6570c937b5aa98e062f5eca030196cc5706cdc
Aymerik <diebold.aymerik@gmail.com>
2023-07-20 18:48:49 +0200

    Added correction

--- traitement.php
+++ traitement.php
@@ -14,4 +14,11 @@ if(empty($_POST['titre'])
     $image = htmlspecialchars($_POST['image']);
 
     // Puis on insère notre oeuvre en base de données
+    require 'bdd.php';
+    $db = connexion();
+    $req = $db->prepare('INSERT INTO oeuvres (titre, description, artiste, image) VALUES (?, ?, ?, ?)');
+    $req->execute([$titre, $description, $artiste, $image]);
+
+    // $db->lastInsertId() permet de récupérer l'id de la dernière ligne insérée en base de données (en l'occurence, l'oeuvre que nous venons d'ajouter).
+    header('Location: oeuvre.php?id=' . $db->lastInsertId());
 }
