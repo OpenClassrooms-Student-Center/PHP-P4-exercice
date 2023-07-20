210535ed0237a1159b42892a111196fabe70b90f
Aymerik <diebold.aymerik@gmail.com>
2023-07-20 18:32:33 +0200

    Added correction

--- oeuvre.php
+++ oeuvre.php
@@ -1,25 +1,20 @@
 <?php
     require 'header.php';
-    require 'oeuvres.php';
+    require 'bdd.php';
+    $db = connexion();
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
     }
 
-    $oeuvre = null;
+    $req = $db->prepare('SELECT * FROM oeuvres WHERE id = ?');
+    $req->execute([intval($_GET['id'])]);
+    $oeuvre = $req->fetch();
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
+    if(!$oeuvre) {
         header('Location: index.php');
     }
 ?>
