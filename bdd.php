d076f26b39a856228f7168f4d73052d4cf2d52d8
Aymerik DIEBOLD <aymerik.diebold@free.fr>
2023-09-11 23:23:32 +0200

    Update bdd.php
    Changement des identifiants de la base de données par défaut

--- bdd.php
+++ bdd.php
@@ -3,7 +3,7 @@ function connexion() {
     try
     {
         // Pensez à modifier cette ligne avec le nom de votre base de données et vos identifiants
-        return new PDO('mysql:host=localhost;dbname=artbox;charset=utf8', 'debian-sys-maint', 'BzbsfkBTXEzhDJ3K');
+        return new PDO('mysql:host=localhost;dbname=artbox;charset=utf8', 'root', '');
     }
     catch (Exception $e)
     {
